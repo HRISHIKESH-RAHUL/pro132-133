80bfe7dd249210d65000d4f87bb083fa126d2de2
HRISHIKESH-RAHUL <76251314+HRISHIKESH-RAHUL@users.noreply.github.com>
2022-01-08 18:29:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function open_bed(){
+    window.location = "bed.html" ;
+}
+
+function open_tv(){
+    window.location = "tvac.html" ;
+}
+
+function open_dsk(){
+    window.location = "desk.html" ;
+}
+
+function open_btl(){
+    window.location = "btl.html" ;
+}
+
+function open_bskt(){
+    window.location = "bskt.html" ;
+}
